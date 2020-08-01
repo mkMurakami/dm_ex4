e2c317de1bd73281729fb02b0048097a0915fc44
mkmk <mkmk@LAPTOP-OAA04AR7.localdomain>
2020-08-01 17:16:14 +0900

    Hot:Initial revision

---
+++ cointos.c
@@ -0,0 +1,30 @@
+//2020.8.1 s1270188
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main(){
+  int coin, h=0, t=0;
+  srand(time(NULL));
+  
+  printf("Tossing a coin...\n");
+
+  for(int i=1;i<=3;i++){
+    printf("Round %d:",i);
+    coin=rand()%2;
+
+    if(coin==0){
+      h++;
+      printf("Heads\n");
+    }
+    else{
+      t++;
+      printf("Tails\n");
+    }
+  }
+
+  printf("Heads:%d, Tails:%d\n",h,t);
+  
+  return 0;
+}
